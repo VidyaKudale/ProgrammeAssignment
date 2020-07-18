c4bb9fbeac4a0bfcd5ed53139d887e0177affe6d
VidyaKudale <68265752+VidyaKudale@users.noreply.github.com>
2020-07-18 13:11:34 +0530

    Create Assign1d.py

---
+++ Assign1d.py
@@ -0,0 +1,8 @@
+#Assignment 1-d
+print("Enter 2 integer numbers:")
+a=input("First Number:")
+b=input("Second Number:")
+a=int(a)
+b=int(b)
+print("Integer Division =",a//b)
+print("Float Division =",a/b)
