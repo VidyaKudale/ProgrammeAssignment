1571ff06e2fdb0a5ec52b110b604cd032a82a971
VidyaKudale <68265752+VidyaKudale@users.noreply.github.com>
2020-07-18 12:57:20 +0530

    Create Assign1c.py

---
+++ Assign1c.py
@@ -0,0 +1,9 @@
+#Assignment 1-c
+print("Enter 2 integer numbers:")
+a=input("First Number:")
+b=input("Second Number:")
+a=int(a)
+b=int(b)
+print("Sum =",a+b)
+print("Difference =",a-b)
+print("Product =",a*b)
