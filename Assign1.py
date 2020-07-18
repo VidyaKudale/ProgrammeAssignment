3584ce140ee45601f3df229bfc787e3fd4d5d3f3
VidyaKudale <68265752+VidyaKudale@users.noreply.github.com>
2020-07-18 12:44:54 +0530

    Update and rename Assign1.py to Assign1b.py

--- Assign1.py
+++
@@ -1,3 +0,0 @@
-# Assignment-1
-#Assign 1-a
-print("Hello,World!")
---
+++ Assign1b.py
@@ -0,0 +1,17 @@
+# Assignment-1
+
+#Assign 1-b
+print("Operations on list :")
+list=[7,4,9,10]
+print("Availble list :",list)
+list.insert(2,5)
+print("After insertion of 5 at index 2 :",list)
+list.remove(7)
+print("After removing 7 from list :",list)
+list.append(11)
+print("Appending 11 in list :",list)
+list.sort()
+print("Sorted list :",list)
+print("Popping element of index 2:",list.pop(2))
+list.reverse()
+print("List in reverse order :",list)
