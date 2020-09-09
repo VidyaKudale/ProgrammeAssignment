94429cf2695e55dad0e312a1224469eaa289f91d
VidyaKudale <68265752+VidyaKudale@users.noreply.github.com>
2020-09-09 23:28:16 +0530

    Update Assign1a.py

--- Assign1a.py
+++ Assign1a.py
@@ -1,6 +1,12 @@
-#Assignment 1-a
-#Display the given array in reverse order
-array=[1,5,2,9,55]
+#Assignment 1
+#Please write a program in C/ C++/ Java/Python   for following statements
+#1. Display the given array in reverse order
+#2. Remove duplicate elements in an array
+#3. Find second max element in given array
+#4. Find sum of elements from given array and find average of all elements in array.
+#5. Check Arrays are disjoint or not( Two arrays are said to be disjoint if they have no elements in common).
+
+array=[1,5,2,9,55,2]
 print("Given Array :",array)
 array.reverse()
 print("After reversing the array :",array)
